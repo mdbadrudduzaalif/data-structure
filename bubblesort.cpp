1a13032e48a8a4e58a5d3031c116f401d0926dbe
Md Badrudduza Alif <85485787+mdbadrudduzaalif@users.noreply.github.com>
2026-02-08 11:03:36 +0600

    Implement bubble sort algorithm in bubblesort.cpp

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -5,6 +5,24 @@ int arr[10]={5,6,8,4,1,2,3,9,7,10};
 int index;
 int last_index;
 
+// Bubble sort algorithm
+for(last_index = 9; last_index > 0; last_index--) {
+    for(index = 0; index < last_index; index++) {
+        // Swap if current element is greater than next element
+        if(arr[index] > arr[index + 1]) {
+            int temp = arr[index];
+            arr[index] = arr[index + 1];
+            arr[index + 1] = temp;
+        }
+    }
+}
 
+// Print sorted array
+cout << "Sorted array: ";
+for(index = 0; index < 10; index++) {
+    cout << arr[index] << " ";
+}
+cout << endl;
 
+return 0;
 }
